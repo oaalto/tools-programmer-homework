eef55f99bdafa32d8285321fe815f83500ed0242
Kalle Soiha <kalle.soiha@metacoregames.com>
2023-10-24 14:27:26 +0300

    Add a README with some instructions. Modify test scaffold a little bit. Add a test 68k binary.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # tools-programmer-homework
+
+Your mission: implement a web service that takes in a set of bytes as input and outputs a Motorola 680x0 processor 
+family disassembly of those bytes as output. For example, assuming the following input bytes:
+
+````
+e7 48 20 20 21 70 21 
+````
+
+the following output (or similar) should be produced:
+
+````
+0x00000000 48e7 2020   MOVEM.L D5,A5,-(A7)
+0x00000004 7021        MOVEQ #$21, D0
+````
+
+You can, based on your own preferences, change the format of the output, but the origin address and bytes used in
+disassembly should always be visible. You can deal with unknown opcodes however you deem appropriate
+(outputting just the hex bytes is sufficient, for example). You should assume the input begins at address 0.
+There are some example binaries in the `test-bin/` directory of the source tree that contain valid 68000 code that
+can be used for testing.
+
+Information on the Motorola 68000 instruction encoding can be found, for example in section 2 of
+https://www.nxp.com/docs/en/reference-manual/M68000PRM.pdf
+
+A simple testing scaffold is provided for testing and development convenience. You can `cargo run` the scaffold in 
+one terminal and then run `cargo test` in another. You can modify the test and add new ones as desired.
+
+For bonus points: implement the possibility to select target processor architecture and assumed code start address 
+along with the input (you don't have to implement additional architectures unless you _really_ want to. If you 
+do, consider the MOS Technology 6510).
--- src/main.rs
+++ src/main.rs
@@ -23,35 +23,36 @@ async fn main() {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Payload {
-    data: [u8; 10],
+    data: Vec<u8>,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Output {
-    data: [u8; 10],
+struct Output { 
+    disassembly: Vec<String>,
 }
 
 async fn handler(Json(payload): Json<Payload>) -> Response {
     let Payload { data } = payload;
-    let res = disassemble(&data);
+    let res = disassemble(data);
     Json(res).into_response()
 }
 
-fn disassemble(_data: &[u8; 10]) -> Output {
+fn disassemble(_data: Vec<u8>) -> Output {
     // process the incoming data here
-    Output { data: [0; 10] }
+    
+        Output { disassembly: ["0x00000000 48e7 2020   MOVEM.L D5,A5,-(A7)","0x00000004 7021        MOVEQ #$21, D0"].iter().map(|&s| s.into()).collect() }
+    
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[tokio::test]
     async fn test_api_disassemble_ok() {
         const URL: &'static str = "http://localhost:9999/";
         let client = reqwest::Client::builder().build().unwrap();
 
-        let payload = Payload { data: [0; 10] };
+        let payload = Payload { data: [0xe7, 0x48, 0x20, 0x20, 0x21, 0x70].to_vec() };
 
         let res: Output = client
             .post(URL)
@@ -61,9 +62,9 @@ mod tests {
             .unwrap()
             .json()
             .await
-            .unwrap();
+            .unwrap(); 
 
-        let expected: Output = Output { data: [0; 10] };
+        let expected: Output = Output { disassembly: ["0x00000000 48e7 2020   MOVEM.L D5,A5,-(A7)", "0x00000004 7021        MOVEQ #$21, D0"].iter().map(|&s| s.into()).collect() };
         assert_eq!(expected, res);
     }
 }
