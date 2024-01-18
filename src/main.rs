bd5a23416f7ebf932f81af6ee253f842e9153d18
Kalle Soiha <kalle.soiha@metacoregames.com>
2024-01-18 13:16:35 +0200

    Update test disassembly to be MOS6510 instead of MC68k

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use axum::{
     response::{IntoResponse, Response},
     routing::post,
@@ -5,6 +6,7 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use std::ops::Add;
 use tracing::info;
 
 #[tokio::main]
@@ -42,8 +44,10 @@ fn disassemble(_data: Vec<u8>) -> Output {
 
     Output {
         disassembly: [
-            "0x00000000 48e7 2020   MOVEM.L D5,A5,-(A7)",
-            "0x00000004 7021        MOVEQ #$21, D0",
+
+            "0x0000 a9 bd        LDA #$bd", 
+            "0x0002 a0 bd        LDY #$bd", 
+            "0x0004 20 28 ba     JSR $ba28"
         ]
         .iter()
         .map(|&s| s.into())
@@ -60,7 +64,7 @@ mod tests {
         let client = reqwest::Client::builder().build().unwrap();
 
         let payload = Payload {
-            data: [0xe7, 0x48, 0x20, 0x20, 0x21, 0x70].to_vec(),
+            data: [0xa9, 0xbd, 0xa0, 0xbd, 0x20, 0x28, 0xba].to_vec(),
         };
 
         let res: Output = client
@@ -75,8 +79,9 @@ mod tests {
 
         let expected: Output = Output {
             disassembly: [
-                "0x00000000 48e7 2020   MOVEM.L D5,A5,-(A7)",
-                "0x00000004 7021        MOVEQ #$21, D0",
+                "0x0000 a9 bd        LDA #$bd",
+                "0x0002 a0 bd        LDY #$bd",
+                "0x0004 20 28 ba     JSR $ba28"
             ]
             .iter()
             .map(|&s| s.into())
